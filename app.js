37a44a480c5f55518a317cb9ec98308d2f974762
Nina Cecilie Højholdt <nhoejholdt@gmail.com>
2016-05-24 21:05:56 +0200

    Remove prismic stuff

--- app.js
+++ app.js
@@ -2,40 +2,39 @@
 /**
  * Module dependencies.
  */
- var express = require('express'),
- favicon = require('serve-favicon'),
- logger = require('morgan'),
- cookieParser = require('cookie-parser'),
- bodyParser = require('body-parser'),
- methodOverride = require('method-override'),
- session = require('express-session'),
- errorHandler = require('errorhandler'),
- http = require('http'),
- path = require('path'),
- prismic = require('express-prismic').Prismic,
- configuration = require('./prismic-configuration').Configuration;
 
+var express = require('express'),
+favicon = require('serve-favicon'),
+logger = require('morgan'),
+cookieParser = require('cookie-parser'),
+bodyParser = require('body-parser'),
+methodOverride = require('method-override'),
+session = require('express-session'),
+errorHandler = require('errorhandler'),
+http = require('http'),
+path = require('path');
 
- var app = express();
-
-// Prismic.io configuration
-
-prismic.init(configuration);
-
+var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
+
+// view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
 app.use(favicon("public/images/punch.png"));
+
 app.use(logger('dev'));
 app.use(bodyParser());
 app.use(methodOverride());
 app.use(cookieParser('1234'));
 app.use(session({secret: '1234', saveUninitialized: true, resave: true}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(errorHandler());
+
 
 function handleError(err, req, res) {
 	if (err.status == 404) {
@@ -47,13 +46,7 @@ function handleError(err, req, res) {
 
 // Routes
 app.route('/').get(function(req, res) {
-	var p = prismic.withContext(req,res);
-	p.getByUID('page', 'front', function (err, pagecontent) {
-		if(err) {
-			return handleError(err, req, res);
-		}
-		res.render('index', { pagecontent: pagecontent });
-	});
+	res.render('index');
 });
 
 
