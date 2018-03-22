05a122bd2bf9b1f205eed87210fa29d1cfff5343
Jesper Fogh <foghjesperjhf@gmail.com>
2018-03-22 13:06:20 +0200

    Added a loading thingy, updated the projects single page, so it has a gallery as well. And working on increasing the speed of the hover background thing

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -10,13 +10,14 @@ $(document).ready(function () {
     }
     var currentPage = hash.substring(1, hash.length);
     if(currentPage.indexOf('projects/') === -1) {
+        toggleLoading(true);
         goToPage(currentPage, false);
     }
     else {
         var currentProject = currentPage.substring('projects/'.length, currentPage.length);
         goToPage('projects', false);
-        getSingleProject(currentProject);
-        showSingleProject();
+        toggleLoading(true);
+        getSingleProject(currentProject, showSingleProject);
     }
 
     // Navigation in main menu
@@ -24,23 +25,24 @@ $(document).ready(function () {
         event.stopPropagation();
         var target = $(this).attr('data-target');
 
+        toggleLoading(true);
         goToPage(target, true, this);
     });
 
     $('body').on('click', '.projects-single-permalink', function() {
         var slug = $(this).attr('data-slug');
-        getSingleProject(slug);
-        showSingleProject();
+        toggleLoading(true);
+        getSingleProject(slug, showSingleProject);
     });
 
     $('body').on('mouseenter', '.projects-single-permalink', function() {
-        var string = $(this).attr('data-name') + ' ';
-        string = string.repeat(500);
-        $('#background-container').html(string);
+        var name = $(this).attr('data-name');
+        $('.background-container[data-name="' + name + '"]').addClass('active');
     });
 
     $('body').on('mouseleave', '.projects-single-permalink', function() {
-        $('#background-container').html('');
+        var name = $(this).attr('data-name');
+        $('.background-container[data-name="' + name + '"]').addClass('active');
     });
 });
 
@@ -62,7 +64,11 @@ function goToPage(target, fromMenu) {
             url: '/' + target,
         }).done(function(data) {
             insertProjectsData(data[target], target, targetSection);
+            toggleLoading(false);
         });
+    }
+    else {
+        toggleLoading(false);
     }
 }
 
@@ -71,7 +77,9 @@ function insertProjectsData(data, type, targetSection) {
 
     // TO DO : Add loading spinner or something
 
-    console.log(data);
+    // console.log(data);
+
+    var bgContainer = $('.background-container');
 
     data.forEach(function(element) {
         var article = $(targetSection).find('article.template').clone();
@@ -83,16 +91,25 @@ function insertProjectsData(data, type, targetSection) {
         $(article).find('.projects-single-paragraph').text(element.fields.leadParagraph);
         $(article).removeClass('template');
         $(targetSection).find('.page-content').append(article);
+
+        var thisBgContainer = bgContainer.clone();
+        thisBgContainer.data('name', element.fields.title);
+        var string = element.fields.title + ''; // Convert to string
+        string = string.repeat(200);
+        thisBgContainer.html(string);
+        $('body').append(thisBgContainer);
     });
 
     targetSection.removeClass('not-loaded').addClass('loaded');
 }
 
-function getSingleProject(slug) {
+function getSingleProject(slug, callback) {
     $.ajax({
         url: '/projects/' + slug,
     }).done(function(data) {
         insertSingleProjectData(data);
+        toggleLoading(false);
+        callback();
     });
 }
 
@@ -102,10 +119,24 @@ function insertSingleProjectData(data) {
     project.find('.projects-single-lead').text(data.project.fields.leadParagraph);
     var bodyHtml = converter.makeHtml(data.project.fields.bodyText);
     project.find('.projects-single-body').html(bodyHtml);
+    $('.projects-single-section .projects-single-img-container').html('');
+    if(data.project.fields.gallery !== undefined && data.project.fields.gallery !== null && data.project.fields.gallery.length > 0) {
+        var imgElem = $('<img/>');
+        for(var i in data.project.fields.gallery) {
+            var imgDat = data.project.fields.gallery[i];
+            var newImg = imgElem.clone();
+            newImg.attr('src', imgDat.fields.file.url);
+            project.find('.projects-single-img-container').append(newImg);
+        }
+    }
     project.removeClass('.projects-single-template').addClass('projects-single').attr('id', data.project.fields.slug);
     $('.projects-single-section .page-content').html(project).removeClass('not-loaded').addClass('loaded');
 }
 
 function showSingleProject() {
     $('.projects-single-section').addClass('active');
+}
+
+function toggleLoading(shouldShow) {
+    shouldShow ? $('.loading-page').addClass('active') : $('.loading-page').removeClass('active');
 }
