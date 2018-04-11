d461aad32a48ec6dc9f29995c6c43744e873fb69
Jesper Fogh <foghjesperjhf@gmail.com>
2018-04-11 18:58:41 +0200

    Made contenful API key super duper secret

--- services/contentfulClient.js
+++ services/contentfulClient.js
@@ -3,7 +3,7 @@ var contentful = require('contentful');
 var client = contentful.createClient({
     space: 'oewqprhhhtof',
     // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-    accessToken: 'a75e18bbc503ef3471d464e9648bfef6cefc1c3ac76338e1fec42d02ba6e718c'
+    accessToken: process.env.CONTENTFUL_API_KEY
 });
 
 exports.client = client;
