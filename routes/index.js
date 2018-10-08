3c0559428e1daead637cc5c5578aac38d340ff16
Nina Cecilie Højholdt <nhoejholdt@gmail.com>
2018-10-08 14:48:40 +0200

    Create custom layout for concepts and add semeion route and view

--- routes/index.js
+++ routes/index.js
@@ -13,4 +13,8 @@ router.get('/contact', function(req, res, next) {
     res.render('pages/contact');
 });
 
+router.get('/concept/semeion', function(req, res, next) {
+    res.render('pages/concept/semeion', { layout: 'concept-layout' });
+})
+
 module.exports = router;
