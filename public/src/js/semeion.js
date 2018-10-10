e099a0d9dbe19b24e63b3a5c7ecaa506aa8ebcfa
Jesper Fogh <foghjesperjhf@gmail.com>
2018-10-10 14:23:20 +0200

    Added tech specs, prepared a video player and added downloads

--- public/src/js/semeion.js
+++ public/src/js/semeion.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-	console.log('READY');
 	$('nav a').click(function(e) {
 		var thisId = $(this).attr('href');
 
@@ -7,5 +6,39 @@ $(document).ready(function() {
         scrollTop: $(thisId).offset().top
     }, 600);
 		e.preventDefault();
+	});
+
+	$('body').on('click', '.concept-video-container .video-wrapper video', function(e) {
+	    e.preventDefault();
+	    var vidElem = $(this).get(0);
+	    if(vidElem.paused) {
+	        vidElem.play();
+	    }
+	    else {
+	        vidElem.pause();
+	    }
+	});
+	$('body').on('click', '.concept-video-container .video-wrapper .mute', function(e) {
+	    e.preventDefault();
+	    var vidElem = $(this).siblings('video').get(0);
+	    if(vidElem.muted) {
+	        vidElem.muted = false;
+	        $(this).addClass('unmuted').removeClass('muted');
+	    }
+	    else {
+	        vidElem.muted = true;
+	        $(this).addClass('muted').removeClass('unmuted');
+	    }
+	});
+	$('body').on('click', '.concept-video-container .video-wrapper .fullscreen', function(e) {
+	    e.preventDefault();
+	    var vidElem = $(this).siblings('video').get(0);
+	    if (vidElem.requestFullscreen) {
+	        vidElem.requestFullscreen();
+	    } else if (vidElem.mozRequestFullScreen) {
+	        vidElem.mozRequestFullScreen();
+	    } else if (vidElem.webkitRequestFullscreen) {
+	        vidElem.webkitRequestFullscreen();
+	    }
 	});
 });
